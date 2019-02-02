e3672be24d429b86a6f408813d2f9f72cf5305e0
Davis W. Frank <dwfrank@gmail.com>
2019-02-02 15:13:24 -0800

    Some refactoring

--- lib/balboa/filename_tools.rb
+++ lib/balboa/filename_tools.rb
@@ -2,42 +2,48 @@ module Balboa
   class NoDateInFilenameError < StandardError; end
 
   MONTH_MAP = {
-    "01" => "01.Jan",
-    "02" => "02.Feb",
-    "03" => "03.Mar",
-    "04" => "04.Apr",
-    "05" => "05.May",
-    "06" => "06.Jun",
-    "07" => "07.Jul",
-    "08" => "08.Aug",
-    "09" => "09.Sep",
-    "10" => "10.Oct",
-    "11" => "11.Nov",
-    "12" => "12.Dec",
+    "01" => "Jan",
+    "02" => "Feb",
+    "03" => "Mar",
+    "04" => "Apr",
+    "05" => "May",
+    "06" => "Jun",
+    "07" => "Jul",
+    "08" => "Aug",
+    "09" => "Sep",
+    "10" => "Oct",
+    "11" => "Nov",
+    "12" => "Dec",
   }
 
   class FilenameTools
     def archive_filename_for(filethis_name)
-      md = filethis_name.match(/(?<doc>.*)(?<year>\d{4})-(?<month>\d{2})-(?<date>\d{2})(?<other>.*)\.pdf/)
+      match = filethis_name.match(/(?<doc>.*)(?<year>\d{4})-(?<month>\d{2})-(?<date>\d{2})(?<other>.*)\.pdf/)
+      raise NoDateInFilenameError unless match
 
-      date = "#{md[:year]}.#{md[:month]}.#{md[:date]}"
-      name = md[:doc].strip.tr(" ", ".").to_s
-      name += ".#{md[:other].strip}" unless md[:other].empty?
+      date = "#{match[:year]}.#{match[:month]}.#{match[:date]}"
+      name = match[:doc].strip.tr(" ", ".").to_s
+      name += ".#{match[:other].strip}" unless match[:other].empty?
 
       "#{date}.#{name}.pdf"
     end
 
-    def month_directory_for(filename)
+    def directory_for(filename)
       year_match = filename.match(/(?<year>\d{4})\.(?<rest>.*)/)
 
       raise NoDateInFilenameError unless year_match
 
-      directory_name = "#{year_match[:year]}/"
-
+      directory_name = [year_match[:year]]
       month_match = year_match[:rest].match(/^(?<month>\d{2}).*/)
-      directory_name += "#{MONTH_MAP[month_match[:month]]}/" if month_match && month_match[:month]
 
+      directory_name << month_dirname(month_match[:month]) if month_match
       directory_name
+    end
+
+    private
+
+    def month_dirname(month_number)
+      [month_number, MONTH_MAP[month_number]].join(".")
     end
   end
 end
--- spec/filename_tools_spec.rb
+++ spec/filename_tools_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe Balboa::FilenameTools do
-  let(:file_this) {Balboa::FilenameTools.new}
+  let(:tools) {Balboa::FilenameTools.new}
 
   describe "#archive_filename_for" do
     context "when the filename is unique" do
       it "returns a filename in the Balboa format" do
-        expect(file_this.archive_filename_for(
+        expect(tools.archive_filename_for(
           "Allstate Automobile 904150241 Statements 2018-07-12.pdf"
         )).
           to eq("2018.07.12.Allstate.Automobile.904150241.Statements.pdf")
@@ -13,10 +13,18 @@ RSpec.describe Balboa::FilenameTools do
 
     context "when the FileThis adds text after the date" do
       it "returns a filename in the Balboa format" do
-        expect(file_this.archive_filename_for(
+        expect(tools.archive_filename_for(
           "Allstate Automobile 904150241 Statements 2018-07-12 4.pdf"
         )).
           to eq("2018.07.12.Allstate.Automobile.904150241.Statements.4.pdf")
+      end
+    end
+
+    context "when given a filename without a recognizable date" do
+      it "fails appropriately" do
+        expect {
+          tools.archive_filename_for("NoDate.File.jpg")
+        }.to raise_error(Balboa::NoDateInFilenameError)
       end
     end
   end
@@ -24,24 +32,28 @@ RSpec.describe Balboa::FilenameTools do
   describe "#month_directory_for" do
     context "when given a filename with a full date" do
       it "returns the directory path" do
-        expect(file_this.month_directory_for("2019.01.12.FooBar.txt")).to eq("2019/01.Jan/")
-        expect(file_this.month_directory_for("2014.05.23.FooBar.txt")).to eq("2014/05.May/")
-        expect(file_this.month_directory_for("2198.11.01.FooBar.txt")).to eq("2198/11.Nov/")
+        expect(tools.directory_for("2019.01.12.FooBar.txt")).to eq(["2019", "01.Jan"])
+        expect(tools.directory_for("2014.05.23.FooBar.txt")).to eq(["2014", "05.May"])
+        expect(tools.directory_for("2198.11.01.FooBar.txt")).to eq(["2198", "11.Nov"])
       end
     end
 
     context "when given a filename with a year" do
       it "returns the directory path" do
-        expect(file_this.month_directory_for("2198.SomeOther.file.txt")).to eq("2198/")
+        expect(tools.directory_for("2198.SomeOther.file.txt")).to eq(["2198"])
       end
     end
 
     context "when given a filename without a recognizable date" do
       it "fails appropriately" do
         expect {
-          file_this.month_directory_for("NoDate.File.jpg")
+          tools.directory_for("NoDate.File.jpg")
         }.to raise_error(Balboa::NoDateInFilenameError)
       end
     end
+  end
+
+  describe "#destination_path" do
+    it ""
   end
 end
