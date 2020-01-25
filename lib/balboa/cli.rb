0bea445af36e806a93802e42334dfd499eace877
Davis W. Frank <dwfrank@gmail.com>
2020-01-25 14:55:49 -0800

    latest

--- lib/balboa/cli.rb
+++ lib/balboa/cli.rb
@@ -5,32 +5,37 @@ require "thor"
 module Balboa
   class CLI < Thor
     desc "archive_filethis FILETHIS_DIR ARCHIVE", "copies all PDFs from FileThis to the archive"
+    method_option :verbose => :boolean, :default => false
     def archive_filethis(source, archive_root)
       puts "Archiving files from #{source} to #{archive_root}"
 
       tools = FilenameTools.new
 
-      Dir.glob("#{source}/**/*.pdf").each do |path_to_pdf|
+      Dir.glob("#{source}/**/*.pdf") # odd bug, need to Dir.glob twice to get the files?
+
+      pdfs = Dir.glob("#{source}/**/*.pdf").sort
+      puts Rainbow("Found #{pdfs.length} PDF files").cyan
+      pdfs.each do |path_to_pdf|
         pdf_name = path_to_pdf.split("/").last
         archive_filename = tools.archive_filename_for(pdf_name)
         year_month_path = tools.directory_for(archive_filename)
 
         unless File.exist? File.join(archive_root, year_month_path)
-          puts "Skipping #{pdf_name} as the archive destination doesn't exist"
+          puts Rainbow("Skipping").red + " #{pdf_name}" + Rainbow(" as the archive destination doesn't exist").red
           next
         end
 
         full_path_to_archived_file = File.join(archive_root, year_month_path, archive_filename)
 
         if File.exist? full_path_to_archived_file
-          puts "Skipping #{pdf_name} as #{archive_filename} is already in the archive"
+          puts "Skipping #{pdf_name} as #{archive_filename} is already in the archive" if options[:versbose]
           next
         end
 
-        puts "Copying #{pdf_name} to #{File.join(year_month_path, archive_filename)}"
+        puts Rainbow("Copying").green + " #{pdf_name}" + Rainbow(" #{File.join(year_month_path, archive_filename)}").green
         FileUtils.cp(path_to_pdf, full_path_to_archived_file)
       rescue
-        puts "Skipping #{pdf_name} as we don't know where to archive it"
+        puts Rainbow("Skipping").red +  " #{pdf_name}" + Rainbow(" as we don't know where to archive it").red
       end
     end
 
