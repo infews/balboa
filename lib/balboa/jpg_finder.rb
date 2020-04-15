2690b5aeeead0c96d1150319a38f0b5096e5e27b
Davis W. Frank <dwfrank@gmail.com>
2020-04-15 16:37:51 -0700

    Removing Finder classes as we can just use Dir.glob

--- lib/balboa/jpg_finder.rb
+++
@@ -1,9 +0,0 @@
-module Balboa
-  class JPGFinder
-    def find(glob)
-      glob.each_with_object([]) do |filename, jpg|
-        jpg << filename if filename =~ /.*(\.jpeg|\.jpg)$/i
-      end
-    end
-  end
-end
--- lib/balboa/pdf_finder.rb
+++
@@ -1,9 +0,0 @@
-module Balboa
-  class PDFFinder
-    def find(glob)
-      glob.each_with_object([]) do |filename, pdfs|
-        pdfs << filename if filename =~ /.*\.pdf$/
-      end
-    end
-  end
-end
